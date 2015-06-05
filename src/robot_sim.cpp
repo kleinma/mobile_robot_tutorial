712ee1c9bc08a0f160939cbe853020b30d603f3e
Matthew Klein <ggallin08@gmail.com>
2015-06-04 20:27:42 -0400

    Added kinematic equations to robot sim. Added transform between robot and map. added robot_sim to launch file. Need to make transform name match robot.

--- src/robot_sim.cpp
+++ src/robot_sim.cpp
@@ -2,16 +2,20 @@
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 #include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
 
-// Create global variable to store the commanded linear and angular velocities. The letter 'u' is used to denote its role in the Kalman filter.
-double g_uv = 0;
-double g_uomega = 0;
+// Create global variable to store the velocities of the left and right wheels
+double g_vl = 0;
+double g_vr = 0;
 
-// Subscribe to the velocity command and store v and omega in 
-void cmd_velCB(const geometry_msgs::Twist& msg)
+// Subscribe to the left and right wheel velocities and store them in a global variable
+void left_wheelCB(const geometry_msgs::Twist& msg)
 {
-  g_uv = msg.linear.x;
-  g_uomega = msg.angular.z;
+  g_vl = msg.linear.x;
+}
+void right_wheelCB(const geometry_msgs::Twist& msg)
+{
+  g_vr = msg.linear.x;
 }
 
 
@@ -24,13 +28,16 @@ int main(int argc, char **argv)
   ros::Publisher robot_statePub = n.advertise<nav_msgs::Odometry>("odom",1);
 
   // Create a subscriber object to subscribe to the topic cmd_vel, which will receive a Twist message that contains the commanded velocity of the robot.
-  ros::Subscriber cmd_velSub = n.subscribe("cmd_vel",1,cmd_velCB);
-
+  ros::Subscriber left_wheelSub = n.subscribe("left_wheel/vel",1,left_wheelCB);
+  ros::Subscriber right_wheelSub = n.subscribe("right_wheel/vel",1,right_wheelCB);
 
   // Create an Odometry message to store the robot state
   // The state consists of x,y in the map frame, theta (or yaw) of the robot frame relative to the map frame, the velocity of the robot in the robot's frame (all in the x_robot direction), and omega, or the change in theta.
   // All of this fits nicely into an Odometry message. The Odometry message provides room for much more. However, we will only populate the fields that we are using.
   nav_msgs::Odometry robot_state;
+  robot_state.child_frame_id = "robot0";
+  robot_state.header.frame_id = "map";
+
   // Create and initialize variables to store current and previous states (p stands for previous)
   // This could be stored in robot_state. But it is easier to use regular variable and then stuff it all back in robot_state at the end.
   double x = 0;
@@ -45,6 +52,14 @@ int main(int argc, char **argv)
   double v_p = v;
   double omega_p = omega;
 
+  double cf; // Correction factor used for numerically integrating x and y
+
+  // Set up the robot parameters and check the parameter server to replace the default value
+  double b = 0.75; // Track width (distance between wheels) in meters
+  n.getParam("b",b);
+
+  double f = 10; // Frequency of loop in Hz
+  double dt = 1/f; // Time step of loop in seconds.
   ros::Rate naptime(10); //create a ros object from the ros “Rate” class; set the sleep timer for 10Hz repetition rate
   while(ros::ok()) // do work here in infinite loop (desired for this example), but terminate if detect ROS has faulted
     {
@@ -54,13 +69,21 @@ int main(int argc, char **argv)
       // Update the state estimate using x_p (the vector) and u
 
       ////////////////////////////////////////////////////////////
-      // This section must be filled in with the correct equations
-      x = x_p + 0;
-      y = y_p + 0;
-      theta = theta_p + 0;
+      // First determine the correction factor. This is described in Wang 1988. The limit of cf as omega approaches 0 is 1. The following if statement protects against dividing by zero.
+      if (omega == 0){
+	cf = 1;
+      }
+      else{
+	cf = sin(omega*dt/2)/(omega*dt/2);
+      }
 
-      v = g_uv;
-      omega = g_uomega;
+      x = x_p + cf*v*dt*cos(theta+omega*dt/2);
+      y = y_p + cf*v*dt*sin(theta+omega*dt/2);
+
+      theta = theta_p + omega*dt;
+
+      v     = (g_vr+g_vl)/2;
+      omega = (g_vr-g_vl)/b; // b is the track width (the width between the wheels)
       ////////////////////////////////////////////////////////////
 
       // Stuff these state variables into our Odometry message, robot_state
@@ -73,12 +96,19 @@ int main(int argc, char **argv)
       // and publish the state. Note that no covariances or 3D fields are used.
       robot_statePub.publish(robot_state);
 
+      // In addition to publishing an odometry message, also broadcast a transform
+      static tf::TransformBroadcaster br;
+      tf::Transform transform;
+      transform.setOrigin( tf::Vector3(x, y, 0.0) );
+      tf::Quaternion q;
+      q.setRPY(0, 0, theta);
+      transform.setRotation(q);
+      br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "robot0"));
+
       // Set the previous state equal to the current state for the next loop
       x_p = x;
       y_p = y;
       theta_p = theta;
-      v_p = v;
-      omega_p = omega;
 
       //next line will cause the loop to sleep for the balance of the desired period to achieve the specified loop frequency
       naptime.sleep();
